00a1ff9bd45216b642b7a7619f447353a00fbec7
jdgc14 <joseojedapro@gmail.com>
2022-09-15 17:20:15 -0500

    adding message final in congratulations page

--- src/components/CongratulationsPage.jsx
+++ src/components/CongratulationsPage.jsx
@@ -10,6 +10,17 @@ const CongratulationsPage = () => {
                 <span className="title-word title-word-3">tula</span>
                 <span className="title-word title-word-4">tions!</span>
             </h3>
+            <div className="col-6 mx-auto fw-bold">
+                <small>
+                    To any sugestion/correction data please send a email to:
+                    pldle.jdgc14@gmail.com. This is a first version, more
+                    funcionality is comming!🥳🥳 <br />
+                    Don't forget to visit{' '}
+                    <a href="https://pybaq.co/" className="link-python">
+                        Python Barranquilla page 💛💙
+                    </a>
+                </small>
+            </div>
         </div>
     )
 }
