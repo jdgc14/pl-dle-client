c6c91d6d58da2ef096520eaa2aab001fc3623635
jdgc14 <joseojedapro@gmail.com>
2022-09-15 19:01:33 -0500

    adding data languages, adding styles

--- programmingLanguages.js
+++ programmingLanguages.js
@@ -82,6 +82,76 @@ const programmingLanguages = [
         level: 'High-Level',
         img: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Cebolla_Chulita.png?20161120043847',
     },
+    {
+        name: 'GO',
+        releaseYear: 2009,
+        typed: ['Static', 'Strong'],
+        plataform: 'Multiplataform',
+        uses: ['Web Development', 'Data Analysis', 'Machine Learning'],
+        paradigms: ['Functional', 'OOP', 'Structured'],
+        level: 'High-Level',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Go_Logo_Blue.svg/640px-Go_Logo_Blue.svg.png',
+    },
+    {
+        name: 'Swift',
+        releaseYear: 2014,
+        typed: ['Static', 'Strong'],
+        plataform: 'MacOS',
+        uses: ['Apple Applications', 'Web Development', 'IoT'],
+        paradigms: ['Functional', 'OOP', 'Declarative'],
+        level: 'High-Level',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Swift_logo.svg/320px-Swift_logo.svg.png',
+    },
+    {
+        name: 'Scala',
+        releaseYear: 2004,
+        typed: ['Static', 'Strong'],
+        plataform: 'JVM',
+        uses: ['Data Analysis', 'Software Development', 'IoT'],
+        paradigms: ['Functional', 'OOP', 'Concurrent'],
+        level: 'High-Level',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Scala-full-color.svg/512px-Scala-full-color.svg.png',
+    },
+    {
+        name: 'Pascal',
+        releaseYear: 1970,
+        typed: ['Static', 'Strong'],
+        plataform: 'Multiplataform',
+        uses: ['Embedded Systems', 'Software Development', 'Developing Games'],
+        paradigms: ['Structured', 'Procedural'],
+        level: 'High-Level',
+        img: 'https://is4-ssl.mzstatic.com/image/thumb/Purple113/v4/d7/24/08/d7240855-0e51-7f7e-6c2d-3bafdea5a05f/source/512x512bb.jpg',
+    },
+    {
+        name: 'Elixir',
+        releaseYear: 2012,
+        typed: ['Dynamic', 'Strong'],
+        plataform: 'Multiplataform',
+        uses: ['Web Development', 'IoT'],
+        paradigms: ['Functional', 'Concurrent', 'Process Oriented'],
+        level: 'High-Level',
+        img: 'https://upload.wikimedia.org/wikipedia/en/a/a4/Elixir_programming_language_logo.png',
+    },
+    {
+        name: 'Haskell',
+        releaseYear: 1990,
+        typed: ['Static', 'Strong'],
+        plataform: 'Multiplataform',
+        uses: ['Web Development', 'Software Development'],
+        paradigms: ['Functional'],
+        level: 'High-Level',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/Haskell-Logo.svg',
+    },
+    {
+        name: 'Kotlin',
+        releaseYear: 2011,
+        typed: ['Static', 'Strong'],
+        plataform: 'Multiplataform',
+        uses: ['Web Development', 'Mobile Development', 'Data Analysis'],
+        paradigms: ['Functional', 'OOP', 'Declarative'],
+        level: 'High-Level',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/d/d4/Kotlin_logo.svg',
+    },
 ]
 
 export default programmingLanguages
@@ -90,31 +160,10 @@ export default programmingLanguages
 //     name: 'PHP',
 // },
 // {
-//     name: 'Swift',
-// },
-// {
 //     name: 'Ruby',
 // },
 // {
-//     name: 'GO',
-// },
-// {
-//     name: 'Kotlin',
-// },
-// {
 //     name: 'Rust',
-// },
-// {
-//     name: 'Pascal',
-// },
-// {
-//     name: 'Elixir',
-// },
-// {
-//     name: 'Haskell',
-// },
-// {
-//     name: 'Scala',
 // },
 // {
 //     name: 'Sql',
